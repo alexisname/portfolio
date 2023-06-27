dbe75f5aeb9076031a646f005a7ccc0894061852
jc <alexchen1990@hotmail.com>
2023-06-26 21:49:03 -0700

    delete about me picture at header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 import ME from '../../assets/me.png'
 import HeaderSocials from './HeaderSocials'
 
@@ -12,13 +14,17 @@ const Header = () => {
         <h1>Junlong Chen</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
-        <HeaderSocials />
-
-        <div className="me">
-          <img src={ME} alt="me" />
+        <div className='header__socials'>
+          <a href="https://www.linkedin.com/in/junlong-chen-9b173322a/" target="_blank"><BsLinkedin /></a>
+          <a href="https://github.com/alexisname?tab=repositories" target="_blank"><FaGithub /></a>
         </div>
+        {/* <HeaderSocials /> */}
 
-        <a href="#contact" className = 'scroll__down'>Scroll Down</a>
+        {/* <div className="me">
+          <img src={ME} alt="me" />
+        </div> */}
+
+        {/* <a href="#contact" className = 'scroll__down'>Scroll Down</a> */}
       </div>
     </header>
   )
