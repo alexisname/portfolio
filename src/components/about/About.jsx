dd2bd2d965decead1378d1aaea1b3b9e8f89fe27
jc <alexchen1990@hotmail.com>
2023-07-21 20:13:51 -0700

    corrected typos

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -23,13 +23,13 @@ const About = () => {
             <article className='about__card'>
               <FaAward className='about__icon'/>
               <h5>Experience</h5>
-              <small>7+ Years Working in International Company</small>
+              <small>7+ Years Working in International Companies</small>
             </article>
 
             <article className='about__card'>
               <FiUsers className='about__icon'/>
               <h5>Credentials</h5>
-              <small>Master of Science in Applied Computer Science (Advaced Computer Programming)</small>
+              <small>Master of Science in Applied Computer Science (Advanced Computer Programming)</small>
             </article>
 
             <article className='about__card'>
@@ -40,7 +40,7 @@ const About = () => {
           </div>
 
           <p>
-          Following the completion of my undergraduate and postgraduate studies in the field of Mechanical Engineering, I have amassed seven years of professional experience in the automotive industry. Throughout this time, I discovered my fervor for software development, leading me to embark upon a Master's degree in Computer Science. As a result, I have acquired the proficiency to construct comprehensive projects, as well as to develop Android applications and games employing Java and C#. 
+          Following the completion of my undergraduate and postgraduate studies in the field of Mechanical Engineering, I have amassed seven years of professional experience in the automotive industry. Throughout this time, I discovered my passion for software development, leading me to embark upon a Master's degree in Computer Science. As a result, I have acquired the proficiency to construct comprehensive projects, as well as to develop Android applications and games employing Java and C#. 
           </p>
 
           <a href="#contact" className='btn btn_primary'>Let's Talk</a>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
+        <h5>Hello I am</h5>
         <h1>Junlong Chen</h1>
         <h5 className="text-light">FullStack Developer</h5>
         <CTA />
